27e6adeb0f996fabfdf72a827acb7130222d4f3a
Oliver Thiele <mail@oliver-thiele.de>
2023-12-08 15:44:06 +0100

    [TASK] Add ext_emconf.php for non-composer installations

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'IRRE Buttons',
+    'description' => 'Adds buttons to content elements with IRRE',
+    'category' => 'fe',
+    'author' => 'Oliver Thiele',
+    'author_email' => 'mail@oliver-thiele.de',
+    'author_company' => 'Web Development Oliver Thiele',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.33-12.4.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
